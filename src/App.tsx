b8f89766c623eac809eb2380c9d642ac3a63d164
anilbeesetti <anilbeesetti10@gmail.com>
2022-11-02 12:18:37 +0530

    added axios, react query packages and initialized query client

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
+import { QueryClient, QueryClientProvider } from "react-query";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import Loginpage from "./pages/Loginpage";
 
+const queryClient = new QueryClient();
+
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LandingPage/>}/>
-        <Route path="/login" element={<Loginpage/>}/>
-      </Routes>
-    </BrowserRouter>
-  )
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<Loginpage />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
+  );
 };
 
 export default App;
