3f94fc8326adae4be34a5c99967d3a37cda9a5a9
anilbeesetti <anilbeesetti10@gmail.com>
2022-11-03 14:44:01 +0530

    added styled button component

---
+++ src/components/PrimaryButton.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+type Props = {
+    children: React.ReactNode,
+    className?: string
+    onClick?: React.MouseEventHandler<HTMLButtonElement> | undefined
+};
+
+const PrimaryButton = ({ children, className, onClick}: Props) => {
+  return (
+    <button 
+    className={`px-3 py-2 bg-primary hover:scale-105 rounded-md text-white transition-all duration-150 ${className}`}
+    onClick = {onClick}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default PrimaryButton;
