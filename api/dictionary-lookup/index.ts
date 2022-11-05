fd786b661d4cd5cef7850ba0b0aeebf3a56d3152
anilbeesetti <anilbeesetti10@gmail.com>
2022-11-05 11:31:08 +0530

    implemented dictionary lookup api

--- api/dictionary-lookup/index.ts
+++ api/dictionary-lookup/index.ts
@@ -1,17 +1,52 @@
-import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import { AzureFunction, Context, HttpRequest } from "@azure/functions";
+import { v4 as uuidv4 } from "uuid";
+const axios = require("axios");
 
-const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    context.log('HTTP trigger function processed a request.');
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+const httpTrigger: AzureFunction = async function (
+  context: Context,
+  req: HttpRequest
+): Promise<void> {
+  const endpoint = "https://api.cognitive.microsofttranslator.com";
+  const location = "centralindia";
+  const API_KEY = process.env["TRANSLATOR_API_KEY"];
 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
+  const response = await axios({
+    baseURL: endpoint,
+    url: "/dictionary/lookup",
+    method: "post",
+    headers: {
+      "Ocp-Apim-Subscription-Key": API_KEY,
+      "Ocp-Apim-Subscription-Region": location,
+      "Content-type": "application/json",
+      "X-ClientTraceId": uuidv4(),
+    },
+    params: {
+      "api-version": "3.0",
+      from: req.query.from,
+      to: [req.query.to],
+    },
+    data: [
+      {
+        text: req.query.word,
+      },
+    ],
+    responseType: "json",
+  });
 
+  console.log(response);
+
+  var responseMessage = "";
+
+  if (response.status == 200) {
+    responseMessage = JSON.stringify(response.data);
+  } else {
+    responseMessage = "";
+  }
+
+  context.res = {
+    // status: 200, /* Defaults to 200 */
+    body: responseMessage,
+  };
 };
 
 export default httpTrigger;
