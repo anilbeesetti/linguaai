11113087c7c9365be7be9f821ad0d13c6667a661
anilbeesetti <anilbeesetti10@gmail.com>
2022-11-07 14:46:07 +0530

    improved resultcard

--- src/components/ResultCard.tsx
+++ src/components/ResultCard.tsx
@@ -24,32 +24,32 @@ const ResultCard = ({
   children,
 }: Props) => {
   return (
-    <div
-      className={`p-4 bg-white rounded-2xl transition shadow-lg w-full relative ${
-        effect &&
-        "before:content-[''] before:absolute before:block before:inset-0 before:bg-slate-600 before:rounded-2xl before:-rotate-2 before:-z-[1]"
-      }`}
-    >
-      <div className="flex flex-row items-center gap-2">
-        {isLoading ? (
-          <div className="flex relative w-7 h-7 rounded-full items-center justify-center">
-            <div className="rounded-full h-6 w-6 bg-primary-100"></div>
-            <div className=" animate-ping absolute inset-0 rounded-full bg-primary-100"></div>
-          </div>
-        ) : (
-          <ResponseInfoIcon isError={isError} isSuccess={isSuccess} />
-        )}
-
-        <div>
-          <h1 className=" font-medium text-sm">{title}</h1>
-          <p className=" text-xs text-gray-500">{subtitle}</p>
-        </div>
-      </div>
-      {isError ? (
-        <div className="mt-3 rounded-md p-2">{error}</div>
-      ) : (
-        isSuccess && <div className="px-3">{children}</div>
+    <div className="w-full relative">
+      {effect && (
+        <div className="absolute block inset-0 bg-slate-600 rounded-2xl -rotate-2"></div>
       )}
+      <div className="rounded-2xl transition shadow-lg w-full relative bg-white">
+        <div className="p-4 flex flex-row items-center gap-2">
+          {isLoading ? (
+            <div className="flex relative w-7 h-7 rounded-full items-center justify-center">
+              <div className="rounded-full h-6 w-6 bg-primary-100"></div>
+              <div className=" animate-ping absolute inset-0 rounded-full bg-primary-100"></div>
+            </div>
+          ) : (
+            <ResponseInfoIcon isError={isError} isSuccess={isSuccess} />
+          )}
+
+          <div>
+            <h1 className=" font-medium text-sm">{title}</h1>
+            <p className=" text-xs text-gray-500">{subtitle}</p>
+          </div>
+        </div>
+        {isError ? (
+          <div className="mt-3 rounded-md p-2">{error}</div>
+        ) : (
+          isSuccess && <div className="px-3">{children}</div>
+        )}
+      </div>
     </div>
   );
 };
